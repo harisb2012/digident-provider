331c191fcce33c60ff30a0b8bcfddff887aeb772
Haris Basic <harisb2012@gmail.com>
2018-10-13 21:21:13 +0200

    Keyboard aware view added

--- Scenes/Verification/DetailsStep.js
+++ Scenes/Verification/DetailsStep.js
@@ -6,6 +6,7 @@ import { DetailsForm } from './forms/DetailsForm';
 import styled from 'styled-components/native';
 import ButtonComponent from 'react-native-button-component';
 import { customShadowStyle } from '../../properties/customShadowStyle';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 const FormWrapper = styled.View`
   background: white;
@@ -22,6 +23,11 @@ const ButtonWrapper = styled.View`
   padding-top: 20;
 `;
 
+const contentContainerStyle = {
+  flexGrow: 1,
+  justifyContent: 'center'
+};
+
 export class DetailsStep extends React.PureComponent {
   render() {
     return (
@@ -34,7 +40,7 @@ export class DetailsStep extends React.PureComponent {
           Please confirm your personal details
         </Text>
 
-        <CenterWrapper>
+        <KeyboardAwareScrollView contentContainerStyle={contentContainerStyle}>
           <FormWrapper style={customShadowStyle}>
             <DetailsForm />
           </FormWrapper>
@@ -45,7 +51,7 @@ export class DetailsStep extends React.PureComponent {
               text="Next"
             />
           </ButtonWrapper>
-        </CenterWrapper>
+        </KeyboardAwareScrollView>
       </VerificationLayout>
     )
   }
